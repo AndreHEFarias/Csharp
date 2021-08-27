2f38d3955b75b7fa15c1694787dcbf5cdd160449
André Farias <andrefalconblue@gmail.com>
2021-08-27 11:35:20 -0300

    Dia 27/08

---
+++ EntitiesEx2/EntitiesEx2/Entities/Post.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course.Entities
+{
+    class Post
+    {
+        public string Titulo { get; set; }
+        public DateTime Moment { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+
+        }
+
+        public Post(string titulo, DateTime moment, string content, int likes)
+        {
+            Titulo = titulo;
+            Moment = moment;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Titulo);
+            sb.Append(Likes);
+            sb.Append(" Curtidas - ");
+            sb.Append(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine("\n"+Content +"\n");
+            foreach(Comment c in Comments)
+            {
+                sb.AppendLine(c.Text);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ EntitiesEx2/EntitiesEx2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Course.Entities;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        Comment c1 = new Comment("Tenha uma boa Viagem");
+        Comment c2 = new Comment("Isso é demais");
+        Post p1 = new Post(
+            "Viagem á Nova Zelandia",
+            DateTime.Parse("21/06/2018 13:56:33"),
+            "Fui visitar esse belo país",
+            12);
+
+        p1.AddComment(c1);
+        p1.AddComment(c2);
+
+        Comment c3 = new Comment("Boa noite");
+        Comment c4 = new Comment("Força");
+        Post p2 = new Post(
+            "Boa noite",
+            DateTime.Parse("22/06/2018 13:56:33"),
+            "Vejo você amanhã",
+            12);
+
+        p1.AddComment(c3);
+        p1.AddComment(c4);
+
+        Console.WriteLine(p1 +"\n"+p2);
+    }
+    }
+
