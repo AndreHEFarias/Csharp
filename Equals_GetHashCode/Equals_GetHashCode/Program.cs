37ddc066afaf11dc35ff53cb3754f1cfb11aeff7
André Farias <andrefalconblue@gmail.com>
2021-09-01 13:46:10 -0300

    Dia 01/09

---
+++ Equals_GetHashCode/Equals_GetHashCode/Program.cs
@@ -0,0 +1,22 @@
+﻿using Equals_GetHashCode.Entities;
+using System;
+
+namespace Equals_GetHashCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Jonas", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Maria", Email = "maria@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a==b);
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+
+
+
+        }
+    }
+}
---
+++ GenericsEx/GenericsEx/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GenericsEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService<string> ps = new PrintService<string>();
+            Console.WriteLine("Quantos valores?");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite os valores:");
+
+            for(int i= 0; i < n; i++)
+            {
+                string x = Console.ReadLine();
+                ps.AddValue(x);
+            }
+            ps.Print();
+            Console.WriteLine("\nPrimeiro: "+ps.First());
+        }
+    }
+}
